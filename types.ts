c8288b8605e197e1784ecc345c19e0d369f0a9f7
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2020-01-06 19:32:42 +0000

    refactoring success and error callbacks for request token, upload file and download file

--- types.ts
+++ types.ts
@@ -4,14 +4,6 @@ type VaultError<E> = (error: E) => Promise<void>
 
 type RequestToken<S extends Success<S>, E extends VaultError<E>> = (url: string, success: S, error: E) => Promise<void>
 
-type UploadFileSuccess<T> = (contents: T) => Promise<void>
+type UploadFile<S extends Success<S>, E extends VaultError<E>> = (fullFilePath: string, success: S, error: E) => Promise<void>
 
-type UploadFileError<T> = (error: T) => Promise<void>
-
-type UploadFile = () => Promise<void>
-
-type DownloadFileSuccess<T> = (content: T) => Promise<void>
-
-type DownloadFileError<T> = (error: T) => Promise<void>
-
-type DownloadFile = () => Promise<void>
+type DownloadFile<S extends Success<S>, E extends VaultError<E>> = (url: string, success: S, error: E) => Promise<void>
