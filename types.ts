9a408a2ce7e963391fd259d2212f4efaa8c8a098
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2020-01-06 16:24:55 +0000

    upload file success / error

--- types.ts
+++ types.ts
@@ -3,3 +3,9 @@ type RequestTokenSuccess = (tokenPath: string, token: string) => Promise<void>
 type RequestTokenError = () => Promise<void>
 
 type RequestToken = (url: string, success: RequestTokenSuccess, error: RequestTokenError) => Promise<void>
+
+type UploadFileSuccess<T> = (contents: T) => Promise<void>
+
+type UploadFileError<T> = (error: T) => Promise<void>
+
+type UploadFile = () => Promise<void>
