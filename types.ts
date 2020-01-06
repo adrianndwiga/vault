cc987d23441ba22e51a549b1e9c61176166a807c
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2020-01-06 15:44:17 +0000

    adding request token types

---
+++ types.ts
@@ -0,0 +1,5 @@
+type RequestTokenSuccess = (path: string, token: string) => Promise<void>
+
+type RequestTokenError = () => Promise<void>
+
+type RequestToken = (url: string, success: RequestTokenSuccess, error: RequestTokenError) => Promise<void>
