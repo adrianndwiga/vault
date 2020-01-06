0e762627bd67aad5d6d4bbf020a667d3523c4d1e
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2020-01-06 20:52:09 +0000

    formatting type.ts

--- types.ts
+++ types.ts
@@ -1,9 +1,12 @@
 type Success<S> = (s: S) => Promise<void>
 
-type VaultError<E> = (error: E) => Promise<void>
+type VaultError<Error> = (error: Error) => Promise<void>
 
-type RequestToken<S extends Success<S>, E extends VaultError<E>> = (url: string, success: S, error: E) => Promise<void>
+type RequestToken<S extends Success<S>, E extends VaultError<E>> = 
+    (url: string, success: S, error: E) => Promise<void>
 
-type UploadFile<S extends Success<S>, E extends VaultError<E>> = (fullFilePath: string, success: S, error: E) => Promise<void>
+type UploadFile<S extends Success<S>, E extends VaultError<E>> = 
+    (fullFilePath: string, success: S, error: E) => Promise<void>
 
-type DownloadFile<S extends Success<S>, E extends VaultError<E>> = (url: string, success: S, error: E) => Promise<void>
+type DownloadFile<S extends Success<S>, E extends VaultError<E>> = 
+    (url: string, success: S, error: E) => Promise<void>
