01816e36b94c96ef1ef3a9d64174df9d9188302f
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2020-01-06 16:38:57 +0000

    added download types and made request token error and success types generic

--- types.ts
+++ types.ts
@@ -1,11 +1,17 @@
-type RequestTokenSuccess = (tokenPath: string, token: string) => Promise<void>
+type RequestTokenSuccess<T> = (token: T) => Promise<void>
 
-type RequestTokenError = () => Promise<void>
+type RequestTokenError<T> = (error: T) => Promise<void>
 
-type RequestToken = (url: string, success: RequestTokenSuccess, error: RequestTokenError) => Promise<void>
+type RequestToken = (url: string, success: RequestTokenSuccess<string>, error: RequestTokenError<string>) => Promise<void>
 
 type UploadFileSuccess<T> = (contents: T) => Promise<void>
 
 type UploadFileError<T> = (error: T) => Promise<void>
 
 type UploadFile = () => Promise<void>
+
+type DownloadFileSuccess<T> = (content: T) => Promise<void>
+
+type DownloadFileError<T> = (error: T) => Promise<void>
+
+type DownloadFile = () => Promise<void>
