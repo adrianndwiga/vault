44b95d15dd175b0c661291887c5f2e41e38c15cb
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2020-01-11 08:06:02 +0000

    adding create and delete bucket from s3 function

--- s3.ts
+++ s3.ts
@@ -1,13 +1,29 @@
 import * as AWS from 'aws-sdk'
 import { readFile, writeFile } from 'fs';
 
-interface FileStore {
-    downloadFile(params: {Bucket: string, Key: string}, filePath: string): Promise<void>
-    uploadFile(params: {Bucket: string, Key: string}, filePath: string): Promise<void>
+interface FileParams {
+
 }
 
-export class AmazonS3 implements FileStore {
-    private readonly s3: AWS.S3;
+interface FileStore {
+    downloadFile(params: FileParams, filePath: string): Promise<void>
+    uploadFile(params: FileParams, filePath: string): Promise<void>
+}
+
+interface CloudDirectory {
+    createDirectory(directoryName: string): Promise<void>
+    deleteDirectory(directoryName: string): Promise<void>
+}
+
+
+interface AWSS3Params extends FileParams {
+    Bucket: string
+    Key: string
+}
+
+export class AWSS3 implements FileStore, CloudDirectory {
+    private readonly s3: AWS.S3
+
     constructor(accessKeyId: string, secretAccessKey: string) {
         this.s3 = new AWS.S3({
             accessKeyId,
@@ -15,7 +31,7 @@ export class AmazonS3 implements FileStore {
         })
     }
 
-    downloadFile(params: {Bucket: string, Key: string}, filePath: string) {
+    downloadFile(params: AWSS3Params, filePath: string) {
         return new Promise<void>((resolve, reject) => {
             this.s3.getObject(params, (err, data) => {
                 writeFile(filePath, data.Body.toString(), 'utf8', (err) => {
@@ -25,7 +41,7 @@ export class AmazonS3 implements FileStore {
         })
     }
 
-    uploadFile(params: {Bucket: string, Key: string}, filePath: string): Promise<void> {
+    uploadFile(params: AWSS3Params, filePath: string): Promise<void> {
         return new Promise<void>((resolve, reject) => {
             readFile(filePath, (err, data) => {
                 if (err) throw err
@@ -38,6 +54,32 @@ export class AmazonS3 implements FileStore {
                         reject(err)
                     resolve()
                 })
+            })
+        })
+    }
+
+    createDirectory(bucketName: string): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            const params = {
+                Bucket: bucketName
+            }
+            this.s3.createBucket(params, (err, data) => {
+                if(err) 
+                    reject(err)
+                resolve()
+            })
+        })
+    }
+
+    deleteDirectory(bucketName: string): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            const params = {
+                Bucket: bucketName
+            }
+            this.s3.deleteBucket(params, (err, data) => {
+                if (err) 
+                    reject(err)
+                resolve()
             })
         })
     }
