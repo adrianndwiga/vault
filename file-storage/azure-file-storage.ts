faab91ba668d839e295729105f860779e6ce9413
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2020-01-12 13:51:18 +0000

    remove private method which is only used once

--- file-storage/azure-file-storage.ts
+++ file-storage/azure-file-storage.ts
@@ -1,5 +1,5 @@
 import { FileParams, FileStore, CloudDirectory } from "./types"
-import { BlobServiceClient, StorageSharedKeyCredential, BlockBlobClient } from '@azure/storage-blob'
+import { BlobServiceClient, StorageSharedKeyCredential } from '@azure/storage-blob'
 import { readFile, createWriteStream } from "fs"
 
 interface AzureFileParams extends FileParams {
@@ -14,11 +14,6 @@ export class AzureFileStorage implements FileStore, CloudDirectory {
         const credential = new StorageSharedKeyCredential(account, accountKey)
         this.blobServiceClient = new BlobServiceClient(
             `https://${account}.blob.core.windows.net`, credential)
-    }
-
-    private getBlockBlobClient(params: AzureFileParams): BlockBlobClient {
-        const containerClient = this.blobServiceClient.getContainerClient(params.containerName)
-        return containerClient.getBlockBlobClient(params.blobName)
     }
 
     createDirectory(directoryName: string): Promise<void> {
@@ -45,7 +40,8 @@ export class AzureFileStorage implements FileStore, CloudDirectory {
         return new Promise<void>((resolve, reject) => {
             readFile(filePath, async (err, data) => {
                 if (err) reject(err)
-                const blockBlobClient = this.getBlockBlobClient(params)
+                const containerClient = this.blobServiceClient.getContainerClient(params.containerName)
+                const blockBlobClient = containerClient.getBlockBlobClient(params.blobName)
                 const uploadResponse = await blockBlobClient.upload(data, data.length)
                 if (uploadResponse._response.status === 200)
                     resolve()
