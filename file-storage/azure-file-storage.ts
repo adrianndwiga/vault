199c40cbe8ac4505aee32394c813a773355e3ce9
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2020-01-12 13:08:12 +0000

    added function to upload file to azure storage

--- file-storage/azure-file-storage.ts
+++ file-storage/azure-file-storage.ts
@@ -1,10 +1,21 @@
-import { FileParams, FileStore, CloudDirectory } from "./types";
+import { FileParams, FileStore, CloudDirectory } from "./types"
+import { BlobServiceClient, StorageSharedKeyCredential } from '@azure/storage-blob'
+import { readFile } from "fs"
 
 interface AzureFileParams extends FileParams {
-    id: string
+    containerName: string, 
+    blobName: string
 }
 
 export class AzureFileStorage implements FileStore, CloudDirectory {
+    private readonly blobServiceClient: BlobServiceClient
+
+    constructor(account: string, accountKey: string) {
+        const credential = new StorageSharedKeyCredential(account, accountKey)
+        this.blobServiceClient = new BlobServiceClient(
+            `https://${account}.blob.core.windows.net`, credential)
+    }
+
     createDirectory(directoryName: string): Promise<void> {
         throw new Error("Method not implemented.");
     }
@@ -15,6 +26,17 @@ export class AzureFileStorage implements FileStore, CloudDirectory {
         throw new Error("Method not implemented.");
     }
     uploadFile(params: AzureFileParams, filePath: string): Promise<void> {
-        throw new Error("Method not implemented.");
+        return new Promise<void>((resolve, reject) => {
+            readFile(filePath, async (err, data) => {
+                if (err) reject(err)
+                const containerClient = this.blobServiceClient.getContainerClient(params.containerName)
+                const blockBlobClient = containerClient.getBlockBlobClient(params.blobName)
+                const uploadResponse = await blockBlobClient.upload(data, data.length)
+                if (uploadResponse._response.status === 200)
+                    resolve()
+                else 
+                    reject(uploadResponse)
+            })    
+        })
     }
 }
